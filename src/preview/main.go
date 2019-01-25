b433b9e556b3832e7e4e10c845f69d3ba0979b00
Quentin Goinaud <armaldio@gmail.com>
2019-01-25 12:24:37 -0500

    v1

---
+++ src/preview/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func runningPath() string {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	exPath := filepath.Dir(ex)
+	return exPath
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		url := os.Args[1]
+
+		fmt.Println(runningPath())
+
+		/* --- */
+		cmd := exec.Command("npm.cmd", "run", "start", "--", url)
+		cmd.Dir = runningPath()
+
+		var stdBuffer bytes.Buffer
+		mw := io.MultiWriter(os.Stdout, &stdBuffer)
+
+		cmd.Stdout = mw
+		cmd.Stderr = mw
+
+		// Execute the command
+		if err := cmd.Run(); err != nil {
+			// log.Panic(err)
+			log.Println(err)
+		}
+
+		log.Println(stdBuffer.String())
+		/* --- */
+
+	} else {
+		fmt.Print("You must pass an url as first argument!")
+	}
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+
+}
